b131524e1e229d24d545e71d060ae15951b4db90
George Olson <georgeolson92@gmail.com>
2022-03-14 11:24:01 -0700

    Formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,21 @@
-
- var elem = $(".fa-terminal");
- var interval = setInterval(function() {
-     if (elem.css('visibility') == 'hidden') {
-         elem.css('visibility', 'visible');
-     } else {
-         elem.css('visibility', 'hidden');
-     }    
- }, 500);
+var elem = $(".fa-terminal");
+var interval = setInterval(function () {
+  if (elem.css("visibility") == "hidden") {
+    elem.css("visibility", "visible");
+  } else {
+    elem.css("visibility", "hidden");
+  }
+}, 500);
 
 $(document).ready(function () {
+  setTimeout(function () {
+    $(".loading-spinner").fadeOut();
+    $(".navbar-nav .nav-item").css("opacity", "1");
+    $(".main-content").css("opacity", "1");
+  }, 1000);
 
-
-        setTimeout(function (){
-            $(".loading-spinner").fadeOut();
-            $(".navbar-nav .nav-item").css("opacity", "1");
-            $(".main-content").css("opacity", "1");
-          }, 1000);
-
-        setTimeout(function (){
-          clearInterval(interval); 
-            elem.css('visibility', 'visible');
-        }, 2000);
-
-
+  setTimeout(function () {
+    clearInterval(interval);
+    elem.css("visibility", "visible");
+  }, 2000);
 });
